695316fdbd02555d54a4d203054f55c1760f3d02
Hannah Zhu <t-hannzhu@microsoft.com>
2022-08-04 18:02:22 -0700

    split a github action into 2

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,7 +30,10 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html', { outputFolder: 'pw-report' }], 
+    ['json', { outputFolder: 'pw-report', outputFile: 'report.json' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -39,7 +42,7 @@ const config: PlaywrightTestConfig = {
      baseURL: 'http://localhost:4280',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
